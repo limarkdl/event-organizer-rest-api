749dce1d124266cec8190ce1914a2c709e86a96a
Ivan Kostin <ivan.kostin@jetbrains.com>
2025-01-19 17:10:58 +0200

    Install TailwindCSS + Serve `public` as static

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 
 app.get("/", (req, res) => {
