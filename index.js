53a01ce945d93e2d282e946221a06e883c23a6d5
Ivan Kostin <ivan.kostin@jetbrains.com>
2025-01-19 19:23:47 +0200

    Connect routing + Setup endpoints + Update DB

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from "express";
 import sequelize from "./api/sequelize.config.js";
+import UserRouter from "./api/routes/UserRouter.js";
+import EventRouter from "./api/routes/EventRouter.js";
+import OrganizerRouter from "./api/routes/OrganizerRouter.js";
+import EventTypeRouter from "./api/routes/EventTypeRouter.js";
+import ReservationRouter from "./api/routes/ReservationRouter.js";
 
 const app = express();
+const API_BASE = '/api'
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
@@ -19,6 +25,12 @@ try {
 } catch (error) {
     console.error('Unable to connect to the database:', error);
 }
+
+app.use(`${API_BASE}/users`, UserRouter)
+app.use(`${API_BASE}/organizer`, OrganizerRouter)
+app.use(`${API_BASE}/event`, EventRouter)
+app.use(`${API_BASE}/eventType`, EventTypeRouter)
+app.use(`${API_BASE}/reservation`, ReservationRouter)
 
 app.listen(3000, () => {
         console.log("Server is running on port 3000");
